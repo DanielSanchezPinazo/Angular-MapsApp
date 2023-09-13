387c6d9bab0d71e75f6841251ef0ed623f4407c3
DanielSanchezPinazo <danielsanchezpinazo@gmail.com>
2023-09-13 14:25:23 +0200

    markers added

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
+
+interface MarkerAndColor {
+  marker: Marker,
+  color: string
+}
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css']
 })
 export class MarkersPageComponent {
+
+  @ViewChild("map")
+  public divMap?: ElementRef;
+
+  public markers: MarkerAndColor[] = [];
+  public map?: Map;
+  public lngLat: LngLat = new LngLat(-4.43, 36.72);
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw "El elemento HTML no se encuentra.";
+
+    this.map = new Map({
+
+      container: this.divMap.nativeElement, // container ID
+
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+
+      center: this.lngLat, // starting position [lng, lat]
+
+      zoom: 13, // starting zoom
+    });
+
+    // const markerHTML = document.createElement("div");
+    // markerHTML.innerHTML = "Daniel Sánchez";
+
+
+    // const marker: Marker = new Marker({
+    //   color: "blue",
+    //   element: markerHTML
+    // })
+    // .setLngLat( [-4.5, 36.7] )
+    // .addTo( this.map );
+  }
+
+  createMarker() {
+
+    if ( !this.map ) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+
+    this.addMarker( lngLat, color );
+  }
+
+  addMarker(lngLat: LngLat, color: string = "red") {
+
+    if ( !this.map ) return;
+
+    const marker: Marker = new Marker({
+      color: color,
+      draggable: true
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+
+    this.markers.push( {marker, color} );
+  }
+
+  deleteMarker( index: number ) {
+
+    this.markers[index].marker.remove();
+    this.markers.splice( index, 1 );
+  }
+
+  flyTo( marker: Marker ) {
+
+    if ( !this.map ) return;
+
+    this.map.flyTo({
+      zoom: 14,
+      center: marker.getLngLat()
+    })
+  }
 
 }
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,9 +1,79 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css']
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild( "map" )
+  public divMap?: ElementRef;
+
+  public zoom: number = 4;
+  public map?: Map;
+  public lngLat: LngLat = new LngLat( -5, 40 );
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw "El elemento HTML no se encuentra.";
+
+    this.map = new Map({
+
+      container: this.divMap.nativeElement, // container ID
+
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+
+      center: this.lngLat, // starting position [lng, lat]
+
+      zoom: this.zoom, // starting zoom
+
+      });
+
+    this.mapListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  mapListeners() {
+
+    if ( !this.map ) throw "Mapa no inicializado.";
+// recuperamos el zoom del mapa
+    this.map.on( "zoom", (ev) => this.zoom = this.map!.getZoom());
+// si el zoom es mayor que 18 vuelve a 18
+    this.map.on( "zoomend", () => {
+
+      if ( this.map!.getZoom() > 18 ) this.map!.zoomTo( 18 )
+    });
+// si el zoom es menor que 0 vuelve a 2
+    this.map.on( "zoomend", () => {
+
+      if ( this.map!.getZoom() < 2 ) this.map!.zoomTo( 2 );
+    });
+// recupera la longitud y latitud
+    this.map.on( "move", () => {
+      this.lngLat = this.map!.getCenter();
+      console.log(this.lngLat);
+
+    })
+  }
+
+  zoomIn() {
+
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+
+    this.map?.zoomOut();
+  }
+// método para conectar con la barra del input
+  zoomChanged( value: string ) {
+
+    this.zoom = Number( value );
+    this.map?.zoomTo( this.zoom );
+  }
 
 }
